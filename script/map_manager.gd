57f868e1f6147d804e9d5fddc4a5b3f678452957
jidosc <oscar.jidemo@hotmail.se>
2026-03-01 13:06:07 +0100

    you can win

--- script/map_manager.gd
+++ script/map_manager.gd
@@ -11,6 +11,7 @@ var player: Player
 func _process(_delta: float) -> void:
 	if player != null:
 		var player_y = player.global_position.y
+		ui.update_depth(player_y)
 		update_chunks(player_y)
 		
 		
