f446d3da6fd66da8471b84fa2739b05706b77044
Kingpoki <alexanderking050501@gmail.com>
2026-03-01 11:19:23 +0100

    ore now spawns in start chunk and has different sprite

--- script/map_manager.gd
+++ script/map_manager.gd
@@ -1,7 +1,5 @@
 class_name MapManager extends Node2D
-
 signal returned_to_surface(player: Player, ore: int)
-
 @export var ui: UI
 var packed_ore: PackedScene = preload("res://scenes/ore.tscn")
 var collected_ore: int = 0
@@ -10,12 +8,9 @@ var chunks = {}
 var chunk_height := 600
 var LOAD_DISTANCE := 2
 var player: Player
-
-
 func _process(_delta: float) -> void:
 	if player != null:
 		var player_y = player.global_position.y
-		ui.update_depth(player_y)
 		update_chunks(player_y)
 		
 		
@@ -26,6 +21,12 @@ func _ready() -> void:
 		player.ui = ui
 	var start_chunk = $start_chunk
 	chunks[0] = start_chunk
+	var surface_offset = 220
+	var chunk_rect = Rect2(
+		Vector2(start_chunk.position.x, start_chunk.position.y + surface_offset),
+		Vector2(400, chunk_height - surface_offset)
+	)
+	distribute_ore(50, chunk_rect)
 	
 func spawn_drill(drill: Player, available_ore: int):
 	collected_ore = available_ore
@@ -35,26 +36,22 @@ func spawn_drill(drill: Player, available_ore: int):
 	
 func reset_drill(drill: Player):
 	returned_to_surface.emit(drill, collected_ore)
-
 func distribute_ore(amount: int, rect : Rect2):
 	if not amount is int:
 		return Error.ERR_INVALID_PARAMETER
 	if rect == null:
 		rect = $Background.get_rect()
 	for i in range(amount):
-		#print(g_rect)
 		var random_pos :Vector2 = Vector2(
 			randi_range(rect.position.x, rect.position.x + rect.size.x),
 			randi_range(rect.position.y, rect.position.y + rect.size.y)
 		)
 		add_ore(random_pos)
-
 func add_ore(at_pos: Vector2):
 	var new_ore: Ore = packed_ore.instantiate()
 	new_ore.position = at_pos
 	new_ore.game_manager = self
 	add_child(new_ore)
-
 func collect_ore(ore: Ore):
 	ore.call_deferred("queue_free")
 	collected_ore += 1
