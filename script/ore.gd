ab1caf2662c1adb58b2e7963bb164dcbaa3ac592
Kingpoki <alexanderking050501@gmail.com>
2026-03-01 12:13:27 +0100

    Added 7 new ores

--- script/ore.gd
+++ script/ore.gd
@@ -1,11 +1,16 @@
 class_name Ore extends Node2D
 
 var game_manager: MapManager
+@export var value : int
+@export var sprite : Texture
+
+func _ready() -> void:
+	$Area2D/Sprite2D.texture = sprite
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		var camera = get_viewport().get_camera_2d()
 		if camera is PlayerCamera:
 			camera._camera_shake()
-		game_manager.collect_ore(self)
+		game_manager.collect_ore(self, value)
 		
